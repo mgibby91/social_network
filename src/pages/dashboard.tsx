2c5517611cfb0a7d9e316b5f50d294a96a640c37
Emily Urdaneta <boss@Emilys-MacBook-Pro.local>
2020-09-22 11:37:16 -0600

    add layout to the project

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ProgressBar from "../components/Button";
+import Button from "../components/Button";
+import useApplicationData from "../hooks/useApplicationData";
+
+const testData = [
+  { bgcolor: "#6a1b9a", completed: 60 },
+  { bgcolor: "#00695c", completed: 30 },
+  { bgcolor: "#ef6c00", completed: 53 },
+];
+
+const addPoints = (data) => {
+  data.map((item) => {
+    console.log(item.completed);
+    item.completed++;
+  });
+};
+
+function Home() {
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview,
+  } = useApplicationData();
+  return (
+    <div className="App">
+      {testData.map((item, idx) => (
+        <ProgressBar
+          key={idx}
+          bgcolor={item.bgcolor}
+          completed={item.completed}
+        />
+      ))}
+      <Button onClick={() => addPoints(testData)}>Completed task</Button>
+    </div>
+  );
+}
+
+export default Home;
