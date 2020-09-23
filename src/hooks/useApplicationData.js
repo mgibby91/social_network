e4614ff0d7383edd5be78aeaddefff69c7febb95
Mathius Johnson <devachievem@gmail.com>
2020-09-23 14:15:27 -0700

    state showing and proper

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -24,21 +24,19 @@ export default function useApplicationData() {
   // RETRIEVES API AND SETS IT WITH REDUCER
   useEffect(() => {
     Promise.all([
-      fetch("http://localhost:8001/api/comments").then((res) => res.json()),
-      fetch("http://localhost:8001/api/comments").then((res) => res.json()),
-      fetch("http://localhost:8001/api/comments"),
-      fetch("/api/posts"),
-      fetch("/api/student_stack"),
-      fetch("/api/tutor_experiences"),
-      fetch("/api/user_profiles"),
-      fetch("/api/users"),
-      fetch("/api/user_profiles/mentor_points/:id"),
-      fetch("/api/user_profiles/student_points/:id"),
+      axios.get("http://localhost:8001/api/comments"),
+      axios.get("http://localhost:8001/api/likes"),
+      axios.get("http://localhost:8001/api/mentor_stack"),
+      axios.get("http://localhost:8001/api/posts"),
+      axios.get("http://localhost:8001/api/student_stack"),
+      axios.get("http://localhost:8001/api/tutor_experiences"),
+      axios.get("http://localhost:8001/api/user_profiles"),
+      axios.get("http://localhost:8001/api/users"),
+      // axios.get("http://localhost:8001/api/user_profiles/mentor_points/:id"),
+      // axios.get("http://localhost:8001/api/user_profiles/student_points/:id"),
     ]).then((all) => {
       console.log("all from applicatin data hook: ", all);
-
       const comments = all[0].data;
-      console.log("fetched comments: ", comments);
       const likes = all[1].data;
       const mentor_stack = all[2].data;
       const posts = all[3].data;
@@ -46,8 +44,8 @@ export default function useApplicationData() {
       const tutor_experiences = all[5].data;
       const user_profiles = all[6].data;
       const users = all[7].data;
-      const mentor_points = all[8].data;
-      const student_points = all[9].data;
+      // const mentor_points = all[8].data;
+      // const student_points = all[9].data;
 
       dispatch({
         type: SET_APPLICATION_DATA,
@@ -59,8 +57,8 @@ export default function useApplicationData() {
         tutor_experiences,
         user_profiles,
         users,
-        mentor_points,
-        student_points,
+        // mentor_points,
+        // student_points,
       });
     });
   }, []);
