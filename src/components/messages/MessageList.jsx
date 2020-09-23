ff9eba68da354df02de9e93f1031c1152da7433c
Matthew Gibson <mattgibson6@gmail.com>
2020-09-23 16:52:48 -0600

    initial message functionality compelte

--- src/components/messages/MessageList.jsx
+++ src/components/messages/MessageList.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import './MessageItem';
+import MessageItem from './MessageItem';
+import '../../styles/messages.css'
 
 export default function MessageList(props) {
 
+  const messageListObj = props.messageList.messageList;
+
+  function isEmpty(obj) {
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop))
+        return false;
+    }
+    return true;
+  }
+
+  const messageListEmpty = isEmpty(messageListObj);
+
+  let messageData;
+
+  if (!messageListEmpty) {
+
+    console.log('hi');
+    messageData = Object.keys(messageListObj).map(messageKey => {
+      return <MessageItem
+        key={Object.keys(messageListObj).indexOf(messageKey)}
+        recentMessage={messageListObj[messageKey][0]}
+        username={messageKey}
+      />
+    })
+  }
+
+
   return (
-    <h1>hi</h1>
+    <div>
+      {messageData ? messageData : null}
+    </div>
   )
 
 }
--- src/pages/messages.jsx
+++ src/pages/messages.jsx
@@ -9,6 +9,8 @@ export default function Messages() {
 
   const [messageList, setMessageList] = useState({});
 
+  console.log('messageList', messageList);
+
   useEffect(() => {
 
     axios.get('http://localhost:8001/api/messages')
@@ -16,9 +18,17 @@ export default function Messages() {
         const currentUserID = data.data.userId;
         const currentData = data.data.data;
 
-        const dateSortedMsgList = messageCleanSort(currentUserID, currentData);
+        const newMessageList = messageCleanSort(currentUserID, currentData);
 
-        console.log('dateSortedMsgList', dateSortedMsgList);
+        console.log('newMessageList', newMessageList);
+
+        setMessageList({ messageList: newMessageList });
+
+        let usernames = [];
+
+        for (let message in newMessageList) {
+          usernames.push(message)
+        }
 
       })
 
@@ -27,7 +37,9 @@ export default function Messages() {
   return (
     <div className='main-message-container'>
       <div className='left-message-container'>
-        <MessageList />
+        <MessageList
+          messageList={messageList}
+        />
       </div>
       <div className='right-message-container'>
 
