142517b9171fa380d73444ef274ed10121ae7ed4
Matthew Gibson <mattgibson6@gmail.com>
2020-09-23 17:18:24 -0600

    change message-time styling

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,11 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/fntcstudent/lighthouse/progress-bar/client/.cache/dev-404-page.js"))),
-  "component---src-pages-404-tsx": hot(preferDefault(require("/Users/fntcstudent/lighthouse/progress-bar/client/src/pages/404.tsx"))),
-  "component---src-pages-dashboard-tsx": hot(preferDefault(require("/Users/fntcstudent/lighthouse/progress-bar/client/src/pages/dashboard.tsx"))),
-  "component---src-pages-index-tsx": hot(preferDefault(require("/Users/fntcstudent/lighthouse/progress-bar/client/src/pages/index.tsx"))),
-  "component---src-pages-user-profile-tsx": hot(preferDefault(require("/Users/fntcstudent/lighthouse/progress-bar/client/src/pages/user-profile.tsx")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/matthewgibson/lighthouse/final-project/social-network/.cache/dev-404-page.js"))),
+  "component---src-pages-404-tsx": hot(preferDefault(require("/Users/matthewgibson/lighthouse/final-project/social-network/src/pages/404.tsx"))),
+  "component---src-pages-dashboard-tsx": hot(preferDefault(require("/Users/matthewgibson/lighthouse/final-project/social-network/src/pages/dashboard.tsx"))),
+  "component---src-pages-index-tsx": hot(preferDefault(require("/Users/matthewgibson/lighthouse/final-project/social-network/src/pages/index.tsx"))),
+  "component---src-pages-messages-jsx": hot(preferDefault(require("/Users/matthewgibson/lighthouse/final-project/social-network/src/pages/messages.jsx"))),
+  "component---src-pages-user-profile-tsx": hot(preferDefault(require("/Users/matthewgibson/lighthouse/final-project/social-network/src/pages/user-profile.tsx")))
 }
 
