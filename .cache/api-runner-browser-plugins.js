597ce0b11ba182569b6b454b597d6b757915fd29
Matthew Gibson <mattgibson6@gmail.com>
2020-09-22 21:23:40 -0600

    update to ERD

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,6 +1,6 @@
 module.exports = [{
       plugin: require('../node_modules/gatsby-plugin-layout/gatsby-browser.js'),
-      options: {"plugins":[],"component":"/Users/boss/lighthouse/social_network/src/Layouts/index.tsx"},
+      options: {"plugins":[],"component":"/Users/matthewgibson/lighthouse/final-project/social-network/src/Layouts/index.tsx"},
     },{
       plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
       options: {"plugins":[],"name":"oah-admin","short_name":"OAH","start_url":"/dashboard","display":"minimal-ui","icon":"src/images/OAH.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"31b663efa53f0c1ef68c5f53f53d55af"},
