026357a6cf119429b8bd44877f3ec5c7a9e78183
Mathius Johnson <devachievem@gmail.com>
2020-09-23 14:07:48 -0700

    almost showing state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,10 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
-import reducer, { SET_POINTS,  SET_APPLICATION_DATA } from "../reducers/application";
+import fetch from "node-fetch";
+import reducer, {
+  SET_POINTS,
+  SET_APPLICATION_DATA,
+} from "../reducers/application";
 
 export default function useApplicationData() {
   const [state, dispatch] = useReducer(reducer, {
@@ -14,53 +18,54 @@ export default function useApplicationData() {
     user_profiles: [],
     users: [],
     mentor_points: 0,
-    student_points: 0,  
+    student_points: 0,
   });
 
-    // RETRIEVES API AND SETS IT WITH REDUCER
-    useEffect(() => {
-      Promise.all([
-        axios.get("/api/comments"),
-        axios.get("/api/likes"),
-        axios.get("/api/mentor_stack"),
-        axios.get("/api/posts"),
-        axios.get("/api/student_stack"),
-        axios.get("/api/tutor_experiences"),
-        axios.get("/api/user_profiles"),
-        axios.get("/api/users"),
-        axios.get("/api/user_profiles/mentor_points/:id"),
-        axios.get("/api/user_profiles/student_points/:id"),
-  
-      ]).then((all) => {
-        console.log("all from applicatin data hook: ", all);
-        const comments = all[0].data;
-        const likes = all[1].data;
-        const mentor_stack = all[2].data;
-        const posts = all[3].data;
-        const student_stack = all[4].data;
-        const tutor_experiences = all[5].data;
-        const user_profiles = all[6].data;
-        const users = all[7].data;
-        const mentor_points = all[8].data;
-        const student_points = all[9].data;
-  
-        dispatch({
-          type: SET_APPLICATION_DATA,
-          comments,
-          likes,
-          mentor_stack,
-          posts,
-          student_stack,
-          tutor_experiences,
-          user_profiles,
-          users,
-          mentor_points,
-          student_points,
-        });
-      });
-    }, []);
+  // RETRIEVES API AND SETS IT WITH REDUCER
+  useEffect(() => {
+    Promise.all([
+      fetch("http://localhost:8001/api/comments").then((res) => res.json()),
+      fetch("http://localhost:8001/api/comments").then((res) => res.json()),
+      fetch("http://localhost:8001/api/comments"),
+      fetch("/api/posts"),
+      fetch("/api/student_stack"),
+      fetch("/api/tutor_experiences"),
+      fetch("/api/user_profiles"),
+      fetch("/api/users"),
+      fetch("/api/user_profiles/mentor_points/:id"),
+      fetch("/api/user_profiles/student_points/:id"),
+    ]).then((all) => {
+      console.log("all from applicatin data hook: ", all);
 
-    console.log("state in hook: ", state);
+      const comments = all[0].data;
+      console.log("fetched comments: ", comments);
+      const likes = all[1].data;
+      const mentor_stack = all[2].data;
+      const posts = all[3].data;
+      const student_stack = all[4].data;
+      const tutor_experiences = all[5].data;
+      const user_profiles = all[6].data;
+      const users = all[7].data;
+      const mentor_points = all[8].data;
+      const student_points = all[9].data;
+
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        comments,
+        likes,
+        mentor_stack,
+        posts,
+        student_stack,
+        tutor_experiences,
+        user_profiles,
+        users,
+        mentor_points,
+        student_points,
+      });
+    });
+  }, []);
+
+  console.log("state in hook: ", state);
   // FOR WEBSOCKET
   useEffect(() => {
     const socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
