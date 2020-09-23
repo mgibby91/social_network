b4032618316a4760508aadf799340e55e401d86a
Matthew Gibson <mattgibson6@gmail.com>
2020-09-23 16:56:30 -0600

    resolve conflict

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,11 +1,11 @@
 var plugins = [{
-      plugin: require('/Users/fntcstudent/lighthouse/progress-bar/client/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      plugin: require('/Users/matthewgibson/lighthouse/final-project/social-network/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/fntcstudent/lighthouse/progress-bar/client/node_modules/gatsby-plugin-layout/gatsby-ssr'),
-      options: {"plugins":[],"component":"/Users/fntcstudent/lighthouse/progress-bar/client/src/Layouts/index.tsx"},
+      plugin: require('/Users/matthewgibson/lighthouse/final-project/social-network/node_modules/gatsby-plugin-layout/gatsby-ssr'),
+      options: {"plugins":[],"component":"/Users/matthewgibson/lighthouse/final-project/social-network/src/Layouts/index.tsx"},
     },{
-      plugin: require('/Users/fntcstudent/lighthouse/progress-bar/client/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/matthewgibson/lighthouse/final-project/social-network/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"oah-admin","short_name":"OAH","start_url":"/dashboard","display":"minimal-ui","icon":"src/images/OAH.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"31b663efa53f0c1ef68c5f53f53d55af"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
