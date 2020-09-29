eb04be6d13b7d753baf7a2b78aebb3205ef2cb7a
Matthew Gibson <mattgibson6@gmail.com>
2020-09-28 20:10:40 -0600

    first working version of username search functionality complete

--- src/components/UsernameSearch/UsernameSearchFeature.jsx
+++ src/components/UsernameSearch/UsernameSearchFeature.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../styles/username-search.css';
 import UsernameSearchItem from './UsernameSearchItem';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function UsernameSearchFeature(props) {
 
@@ -11,6 +12,7 @@ export default function UsernameSearchFeature(props) {
     return <UsernameSearchItem
       key={index}
       name={result}
+      setUsernameValue={setUsernameValue}
     />
   })
 
@@ -21,11 +23,12 @@ export default function UsernameSearchFeature(props) {
     console.log(userInput);
 
     // user has to input set number of chars
-
     let currentSearchResults = [];
-    for (let username of usernameList) {
-      if (username.includes(userInput)) {
-        currentSearchResults.push(username);
+    if (userInput.length > 0) {
+      for (let username of usernameList) {
+        if (username.includes(userInput)) {
+          currentSearchResults.push(username);
+        }
       }
     }
 
@@ -33,11 +36,23 @@ export default function UsernameSearchFeature(props) {
     console.log(currentSearchResults);
   }
 
+  function setUsernameValue(username) {
+
+    const searchInput = document.querySelector('#search-user-input');
+    searchInput.value = username;
+
+    setSearchResults([]);
+  }
+
   return (
     <div className='username-search-container'>
       <div className='username-search-input'>
         <label htmlFor="search-user">Search: </label>
-        <input type="text" id='search-user-input' placeholder='Enter username...' onChange={() => changeSearchState()} />
+        <input
+          type="text"
+          id='search-user-input'
+          placeholder='Enter username...'
+          onChange={() => changeSearchState()} />
       </div>
       <div className="username-search-results">
         {searchResultData}
--- src/components/UsernameSearch/UsernameSearchItem.jsx
+++ src/components/UsernameSearch/UsernameSearchItem.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function UsernameSearchItem(props) {
 
   return (
-    <div>{props.name}</div>
+    <div className='username-search-results-item' onClick={() => props.setUsernameValue(props.name)}>
+      {props.name}
+    </div>
   )
 
 }
