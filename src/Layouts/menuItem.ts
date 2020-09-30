2f76b355aef9a2abc0079b166385b677fe8e7b3d
Mathius Johnson <devachievem@gmail.com>
2020-09-30 13:31:45 -0700

    needs posts fixed, and route to messsages from mentors

--- src/Layouts/menuItem.ts
+++ src/Layouts/menuItem.ts
@@ -12,10 +12,6 @@ const items: MenuItemType[] = [
     link: { to: "/user-profiles" },
   },
   {
-    title: "Login-Logout",
-    link: { to: "/login-logout" },
-  },
-  {
     title: "Messages",
     icon: { name: "message-square-outline" },
     link: { to: "/messages" },
