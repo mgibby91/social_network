13b643f9855ca8dfc24d5b9cfbc2807981e99d67
Mathius Johnson <devachievem@gmail.com>
2020-10-01 11:00:19 -0700

    creating user list

--- src/components/Profile/UserProfileItem.jsx
+++ src/components/Profile/UserProfileItem.jsx
@@ -34,7 +34,7 @@ function UserProfileItem(props) {
     back();
   }
 
-  // const users = state.users;
+  console.log(state.users);
   let currentUser = state.users.find(
     (user) => user.id === props.userId || user.username === props.userId
   );
@@ -43,35 +43,25 @@ function UserProfileItem(props) {
     <>
       <ContextConsumer>
         {({ data, set }) => {
-          console.log("data in context: ", data.state.users);
           if (!data.state) return null;
+          console.log("data in context: ", data.state.users);
 
           if (!currentUser) {
-            currentUser = data.state.mentor_points.find(
+            currentUser = data.state.users.find(
               (user) => user.username === data.selected
             );
-            // console.log("current user in context: ", currentUser);
+            console.log("current user in context: ", currentUser);
           }
-          // console.log("mentor points: ", data.state.mentor_points);
-          if (!currentUser) {
-            currentUser = data.state.student_points.find(
-              (user) => user.username === data.selected
-            );
-            // console.log("current user in context: ", currentUser);
-          }
-          // console.log("student points: ", data.state.student_points);
 
           if (!currentUser) {
             return <h1>You must be logged in to view this page.</h1>;
           }
-          // console.log("current user in item: ", currentUser.id);
-          // console.log("current user in item: ", currentUser.student_id);
+
           if (currentUser.id || currentUser.student_id || currentUser.mentor_id)
             senderID = currentUser;
-          // console.log("sender id in context: ", senderID.id);
 
           const posts = getUserPosts(state.posts, senderID.id);
-          // console.log("posts in prof item: ", posts);
+
           const user = getUser(state.user_profiles, senderID.id);
 
           const mentor = data.state.mentor_points.find(
@@ -82,15 +72,8 @@ function UserProfileItem(props) {
             (student) => student.id === user.id
           );
 
-          // console.log("mentor, student: ", mentor, student);
-
           const mentor_stack = getStack(state.mentor_stack, senderID.id);
-          // console.log("data student points ", data.state.student_points);
-          // console.log("data mentor points ", data.state.mentor_points);
 
-          // console.log("user in prof item: ", user);
-          // console.log("sender id in prof item: ", senderID.id);
-          // console.log("mentor rating in user item: ", currentUser);
           return (
             <Row>
               <Col breakPoint={{ xs: 12 }}>
---
+++ src/pages/user-list.tsx
@@ -0,0 +1,30 @@
+import { Card, CardBody } from "@paljs/ui/Card";
+import Row from "@paljs/ui/Row";
+import Col from "@paljs/ui/Col";
+import React from "react";
+import Mentors from "../components/MentorList/Application";
+import useApplicationData from "../hooks/useApplicationData";
+
+export default function MentorList() {
+  const { state, setSelectedUser } = useApplicationData();
+
+  const mentors = state.mentor_points;
+  
+  return (
+    <>
+      <Row>
+        <Col breakPoint={{ xs: 12 }}>
+          <Card>
+            <CardBody>
+              <Mentors 
+                mentors={mentors}
+                setSelectedUser={setSelectedUser}
+                mentor_stack={state.mentor_stack}
+              />
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+}
