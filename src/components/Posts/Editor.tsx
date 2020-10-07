3b7f2caef88f41b7060eb35213b673b10caf2f1a
Mathius Johnson <devachievem@gmail.com>
2020-10-06 22:53:11 -0700

    fixing editor

--- src/components/Posts/Editor.tsx
+++ src/components/Posts/Editor.tsx
@@ -64,8 +64,11 @@ function Editor(props) {
           return (
             <>
             <div className="flex">
-              <MDEditor className="editor" value={value} onChange={setValue} />  
+              <MDEditor className="editor" value={value} onChange={setValue} />
               <div className="flex-row">
+                <div className="tags">
+                  <Tags suggested={props.suggestion} onChange={onChangeInput} />
+                </div>
                 <div className="checkbox">
                   <Checkbox
                     checked={checkbox[1]}
@@ -74,9 +77,9 @@ function Editor(props) {
                   >
                     Mentor Help Needed
                   </Checkbox>
-                </div>
-                <div className="tags">
-                  <Tags suggested={props.suggestion} onChange={onChangeInput} />
+                  <div className="post-btn" onClick={() => onSave()}>
+                    Post
+                  </div>
                 </div>
               </div>
               <Col key={1} offset={{ xs: 11 - 1 }} breakPoint={{ xs: 1 + 1 }}>
@@ -91,7 +94,6 @@ function Editor(props) {
             <div>
               <section className="validation">{error}</section>
             </div>
-
             </>
           );
         }}
